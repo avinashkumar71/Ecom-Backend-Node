db2ca6150278eb341d9a5ac9221682da11e1244d
Avinash Kumar <119006127+avinashkumar71@users.noreply.github.com>
2024-09-07 13:59:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const corsOrigin = ['https://ecommerce-m19c.onrender.com','http://localhost:3000'];
+const corsOrigin = 'https://ecommerce-m19c.onrender.com';
 app.use(cors({
   origin:[corsOrigin],
   methods:['GET','POST','DELETE','PUT'],
