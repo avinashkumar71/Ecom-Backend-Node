28f376561b26c89fa6f4b580a109c62647f89825
Avinash Kumar <avinashkumarprasad72@gmail.com>
2024-09-11 18:52:52 +0530

    v6

--- database/order.js
+++ database/order.js
@@ -3,7 +3,10 @@ const OrderSchema = new mongoose.Schema({
     payment_request_id:String,
     user_id:String,
     amount:Number,
-    payment_status:String
+    payment_status:String,
+    products:Object,
+    address:Object,
+    payment_mode:String
 });
 
 module.exports = mongoose.model("order",OrderSchema)
--- database/orderProduct.js
+++ database/orderProduct.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const OrderProductSchema = new mongoose.Schema({
     payment_request_id:String,
     products:Object,
-    address:Object
+    address:Object,
+    payment_mode:String
 });
 
 module.exports = mongoose.model("orderProduct",OrderProductSchema)
